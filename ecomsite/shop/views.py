fdd8fc6510b21c7a409883ac12eee8ba3f4dc595
Anshul <lit2019054@iiitl.ac.in>
2021-07-24 16:53:22 +0530

    Adding the Cart and Checkout Features

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -10,7 +10,7 @@ def index(request):
     if item_name != '' and item_name is not None:
         product_objects = product_objects.filter(title__icontains=item_name)
 
-    paginator = Paginator(product_objects,3)
+    paginator = Paginator(product_objects,4)
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
 
@@ -19,3 +19,6 @@ def index(request):
 def detail(request,id):
     product_object = Product.objects.get(id=id)
     return render(request, 'shop/detail.html',{'product_object':product_object})
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')
